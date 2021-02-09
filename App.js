ee539f44d8b2e4136b6ce7b94ff813c3074ecc57
GabrielRalph <gltralph@gmail.com>
2021-02-09 20:18:11 +1100

    loader back

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {Windows} from './Utilities/Windows.js'
 import {Controls} from './Utilities/Controls.js'
-// import {MCMLoader} from './Utilities/MCMLoader.js'
+import {MCMLoader} from './Utilities/MCMLoader.js'
 import {Content} from './Contents/Content.js'
 import {Admin} from './Admin/Admin.js'
 import {User} from './Utilities/User.js';
@@ -9,7 +9,7 @@ class App extends Windows{
   constructor(parent){
     super('div');
     this.class = "app"
-    // this.loader = new MCMLoader();
+    this.loader = new MCMLoader();
     this.loader.start();
 
     this.user = new User();
