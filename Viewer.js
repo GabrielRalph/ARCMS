033a6cd65c187d24ab4cf56730868371e1137fc5
GabrielRalph <gltralph@gmail.com>
2021-02-24 13:42:28 +1100

    done

--- Viewer.js
+++ Viewer.js
@@ -184,7 +184,7 @@ class Viewer extends SvgPlus{
       'orientation': '0deg 0deg 25deg',
       'exposure': 0.9,
       'field-of-view': '30deg',
-      'image': '../Assets/scene3.hdr',
+      'environment-image': '../Assets/scene3.hdr',
       style: {
         width: "100%",
         height: "100%"
